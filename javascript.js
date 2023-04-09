3a521e751b4aef48288d7ca9ee8a69f1634f1702
kb419940 <s102321044@gmail.com>
2023-04-09 19:57:07 +1000

    9 Apr. Create 16*16 grid of square divs [O] container didn't set in centre. A: Put a container into another flex div. [O] cells 1-16 wrapped inside the row div. A: Cancel div.row css

--- javascript.js
+++ javascript.js
@@ -1,5 +1,28 @@
 const container = document.querySelector('#container');
-const content = document.createElement('div');
 
-content.classList.add('box')
-container.appendChild(content);
+
+
+for (let i = 1; i <= 16; i++) {
+    const row = document.createElement('div');
+    row.classList.add('row');
+    row.setAttribute('key', `row-${i}`);
+
+    
+    // container.appendChild(row); 
+    for (let index = 1; index <= 16; index++) {
+        const cell = document.createElement('div');
+        cell.classList.add('cell');
+        cell.setAttribute('key', `cell-${index}`);
+        
+        row.appendChild(cell);    
+    }
+    container.appendChild(row);    
+}
+
+
+
+
+
+
+
+
